43640251dadc9a15116baa24c5ae25f8b183e5ac
mianfremo <xmianfremox@gmail.com>
2020-11-25 17:45:14 -0400

    Agregada carpeta build

---
+++ build/precache-manifest.0493fdbc41e926b72cf2f5b998e5e1b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ed042e68fac2b1cfdce99eb6299c0b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1739d69a82b67491c02c",
+    "url": "/static/css/main.c6001462.chunk.css"
+  },
+  {
+    "revision": "24168d38be39d4811542",
+    "url": "/static/js/2.350468a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.350468a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1739d69a82b67491c02c",
+    "url": "/static/js/main.d474deb9.chunk.js"
+  },
+  {
+    "revision": "eef77dccbcadaec63a41",
+    "url": "/static/js/runtime-main.3a3d484f.js"
+  }
+]);
