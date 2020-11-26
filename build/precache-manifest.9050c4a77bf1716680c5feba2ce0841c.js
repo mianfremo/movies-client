c2381ef5f1ce1c323c763dbbe74066c9a11863ae
mianfremo <xmianfremox@gmail.com>
2020-11-25 21:09:27 -0400

    Limpieza a archivos inutilizados

---
+++ build/precache-manifest.9050c4a77bf1716680c5feba2ce0841c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c46ac6caa9c0d87edcc5e7c56567778",
+    "url": "/movies-client/index.html"
+  },
+  {
+    "revision": "f67f7fd002036e2d2377",
+    "url": "/movies-client/static/css/main.c6001462.chunk.css"
+  },
+  {
+    "revision": "24168d38be39d4811542",
+    "url": "/movies-client/static/js/2.350468a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movies-client/static/js/2.350468a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f67f7fd002036e2d2377",
+    "url": "/movies-client/static/js/main.874a9606.chunk.js"
+  },
+  {
+    "revision": "16b2879d4aac40b800e8",
+    "url": "/movies-client/static/js/runtime-main.4cfb8598.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0493fdbc41e926b72cf2f5b998e5e1b7.js"
+  "/movies-client/precache-manifest.9050c4a77bf1716680c5feba2ce0841c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movies-client/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
